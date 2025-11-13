e476e9b8c33aefb1e8654c8da459e342e653371f
Elias Titian <titi8331@rscloud.ca>
2025-11-13 14:09:15 -0500

    Created Import and Public ShooterParameters in ShooterParameters.java

--- src/main/java/frc/robot/subsystems/ShooterParameters.java
+++ src/main/java/frc/robot/subsystems/ShooterParameters.java
@@ -1,6 +1,10 @@
 // The Subsystem ShooterParameters class holds all of the constants and parameters for the ShooterSubsystem
 package frc.robot.subsystems;
 
-public class ShooterParameters {
- // Put all of the ShooterSubsystem parameters here
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterParameters extends ShooterSubsystem {
+    public ShooterParameters() {
+        // Initialize all of the ShooterSubsystem parameters here
+    }
 }
