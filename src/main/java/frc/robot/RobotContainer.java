6d5d00e751aa401b7cd25d0c3a9122d65984c226
Elias Titian <titi8331@rscloud.ca>
2025-11-18 16:18:01 -0500

    TunerConstants Completed - Plus additional small things in other files

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,132 +1,83 @@
-// RobotContainer.java is where everything is defined and configured for the robot
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-// All imports will be between Lines 6, 8-18, 20-42, 44-65, 67-78, 79-84
 import static edu.wpi.first.units.Units.*;
-// All Frc imports Lines 8-18
-import frc.robot.subsystems.*;
-import frc.robot.commands.ShooterAuto;
-import frc.robot.commands.ShooterCommand;
-import frc.robot.commands.IntakeAuto;
-import frc.robot.commands.IntakeCommand;
-import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.subsystems.ShooterParameters;
-import frc.robot.subsystems.IntakeParameters;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.generated.TunerConstants.OperatorConstants;
-// All WPILib imports Lines 6, 20-42
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.apriltag.*;
-import edu.wpi.first.apriltag.jni.*;
-import edu.wpi.first.units.Units;
-import edu.wpi.first.math.*;
-import edu.wpi.first.math.geometry.*;
-import edu.wpi.first.math.geometry.Pose2d;
+
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.*;
-import edu.wpi.first.math.util.*;
-import edu.wpi.first.math.jni.*;
-import edu.wpi.first.units.*;
-import edu.wpi.first.units.Units.*;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.sysid.*;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.simulation.*;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.drive.*;
-import edu.wpi.first.wpilibj.motorcontrol.*;
-// All CTRE Phoenix imports Lines 44-65
-import com.ctre.phoenix6.configs.*;
-import com.ctre.phoenix6.configs.jni.*;
-import com.ctre.phoenix6.swerve.*;
-import com.ctre.phoenix6.swerve.jni.*;
-import com.ctre.phoenix6.wpiutils.*;
-import com.ctre.phoenix6.Orchestra;
-import com.ctre.phoenix6.SignalLogger;
-import com.ctre.phoenix6.Utils;
-import com.ctre.phoenix6.signals.*;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import com.ctre.phoenix6.hardware.jni.*;
-import com.ctre.phoenix6.hardware.core.*;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix.*;
-import com.ctre.phoenix.time.StopWatch;
-import com.ctre.phoenix.motorcontrol.*;
-import com.ctre.phoenix.motion.*;
-import com.ctre.phoenix.time.*;
-// All PathPlanner imports Lines 67-78
-import com.pathplanner.lib.*;
-import com.pathplanner.lib.commands.*;
-import com.pathplanner.lib.auto.*;
-import com.pathplanner.lib.auto.NamedCommands;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.FollowPathCommand;
-import com.pathplanner.lib.commands.PathPlannerAuto;
-import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.util.*;
-import com.pathplanner.lib.util.swerve.*;
-import com.pathplanner.lib.pathfinding.*;
-import com.pathplanner.lib.path.*;
-// All RevRobotics imports Lines 80-84
-import com.revrobotics.jni.*;
-import com.revrobotics.config.*;
-import com.revrobotics.spark.*;
-import com.revrobotics.spark.config.*;
-import com.revrobotics.*;
+import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
-
- // This class is where the bulk of the robot should be declared. Since Command-based is a
- // "Declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- // Periodic methods (other than the Scheduler Calls). Instead, the structure of the robot
- // (including Subsystems, Commands, and Trigger Mappings) should be declared here.
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final IntakeSubsystem m_IntakeSubsystem = new IntakeSubsystem();
-  private final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
-  // Replace with CommandPS4Controller or CommandJoystick if need be
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-  // The container for the robot. Contains subsystems, OI devices, and commands.
-  public RobotContainer() {
-    // Configure the trigger bindings inside this method.
-    configureBindings();
-    new Trigger(m_ShooterSubsystem::ShooterCondition)
-        .onTrue(new ShooterCommand(m_ShooterSubsystem));
-    
-    m_driverController.b().whileTrue(m_ShooterSubsystem.ShooterMethodCommand());
-  }
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
-  // Use this method to define your trigger->command mappings. Triggers can be created via the
-  // {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-  // predicate, or via the named factories in {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s 
-  // subclasses for {@link CommandXboxController Xbox}
-  // {@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller PS4} controllers or 
-  // {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight joysticks}.
+    private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  private void configureBindings() {
-    // Schedule 'IntakeCommand' when 'IntakeCondition' changes to 'true'
-    new Trigger(m_IntakeSubsystem::IntakeCondition)
-        .onTrue(new IntakeCommand(m_IntakeSubsystem));
-    // Schedule 'IntakeMethodCommand' when the Xbox controller's A button is pressed,
-    // cancelling on release.
-    m_driverController.a().whileTrue(m_IntakeSubsystem.IntakeMethodCommand());
-  }
+    private final CommandXboxController joystick = new CommandXboxController(0);
 
-  // Use this to pass the autonomous command to the main {@link Robot} class.
-  // @return the command to run in autonomous
-  public Command getAutonomousCommand() {
-    // The Intake command will be run in autonomous mode
-    return IntakeAuto.IntakeAuto(m_IntakeSubsystem);
-  }
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
+
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {
+        // Note that X is defined as forward according to WPILib convention,
+        // and Y is defined as to the left according to WPILib convention.
+        drivetrain.setDefaultCommand(
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() ->
+                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+            )
+        );
+
+        // Idle while the robot is disabled. This ensures the configured
+        // neutral mode is applied to the drive motors while disabled.
+        final var idle = new SwerveRequest.Idle();
+        RobotModeTriggers.disabled().whileTrue(
+            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+        );
+
+        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        joystick.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        ));
+
+        // Run SysId routines when holding back/start and X/Y.
+        // Note that each routine should be run exactly once in a single log.
+        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+
+        // reset the field-centric heading on left bumper press
+        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+
+        drivetrain.registerTelemetry(logger::telemeterize);
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }
